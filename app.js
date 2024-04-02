f7f883e88c01f49fde72c56518ea890c68d5b1f9
rahulluthra21 <rahulluthra21@gmail.com>
2024-04-02 19:44:54 +0530

    second commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(errorHandlerMiddlerware);
 //   res.send("<br><hr><h1>Task Manager App</h1><hr>");
 // });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
